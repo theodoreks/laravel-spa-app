de9520868aca02ae26675ccdee72e3d58bddb3f5
Theodoreks <scropia2005@gmail.com>
2025-07-29 07:53:44 +0700

    b

--- resources/views/Owner/laporaninventory.blade.php
+++ resources/views/Owner/laporaninventory.blade.php
@@ -14,9 +14,9 @@
   <div class="flex justify-between items-center mb-4">
     <h2 class="text-lg font-medium">Laporan Inventory</h2>
     <div class="flex space-x-2">
-      <a href="{{ route('owner.inventory.create') }}" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+      <!-- <a href="{{ route('owner.inventory.create') }}" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
         + Tambah
-      </a>
+      </a> -->
       <a href="{{ route('owner.inventory.export') }}" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700">
         <i class="fas fa-file-download mr-1"></i> Unduh
     </a>
